e013a7eb8104f197fa1c2ccffddb4916b5c42421
theExile015 <theExile015@gmail.com>
2021-10-03 20:45:15 +0300

    L11. Partially done

---
+++ Assets/PixelCrew/Components/SpawnComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixeCrew.Components
+{
+
+    public class SpawnComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _target;
+        [SerializeField] private GameObject _prefub;
+
+        [ContextMenu("Spawn")]
+
+        public void Spawn()
+        {
+            Instantiate(_prefub, _target);
+        }
+    }
+}
