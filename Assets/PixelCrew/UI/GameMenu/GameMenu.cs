f90c617eba19fac05234f9ea3fdd65167b494a02
theExile015 <theExile015@gmail.com>
2022-02-02 16:51:38 +0300

    UI_bug_fixed

---
+++ Assets/PixelCrew/UI/GameMenu/GameMenu.cs
@@ -0,0 +1,47 @@
+﻿using PixelCrew.UI;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.PixelCrew.UI.GameMenu
+{
+    public class GameMenu : AnimatedWindow
+    {
+
+        private System.Action _closeAction;
+
+        public void OnShowSettings()
+        {
+            var window = Resources.Load<GameObject>("UI/SettingsWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+
+        public void OnRetartGame()
+        {
+            _closeAction = () => { SceneManager.LoadScene("Level2"); };
+            Close();
+        }
+
+        public void OnExit()
+        {
+            _closeAction = () =>
+            {
+                Application.Quit();
+
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            };
+
+            Close();
+        }
+
+        public override void OnCloseAnimationComplete()
+        {
+            base.OnCloseAnimationComplete();
+            _closeAction?.Invoke();
+
+        }
+    }
+}
---
+++ Assets/PixelCrew/UI/GameMenuButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.UI
+{
+    public class GameMenuButton : MonoBehaviour
+    {
+        // Update is called once per frame
+        public void OnGameMenuButtonClick()
+        {
+            var window = Resources.Load<GameObject>("UI/GameMenuWindow");
+            var canvas = FindObjectOfType<Canvas>();
+            Instantiate(window, canvas.transform);
+        }
+    }
+}
