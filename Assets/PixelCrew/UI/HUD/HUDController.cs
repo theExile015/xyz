18a4d2245762675bbaab476800d5e7df81f4ffa1
theExile015 <theExile015@gmail.com>
2022-02-13 22:22:28 +0300

    HUD_001

---
+++ Assets/PixelCrew/UI/HUD/HUDController.cs
@@ -0,0 +1,12 @@
+﻿using PixelCrew.UI.Widgets;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.PixelCrew.UI.HUD
+{
+    public class HUDController : MonoBehaviour
+    {
+        [SerializeField] private ProgressBarWidget _healthBar;
+
+    }
+}
---
+++ Assets/PixelCrew/UI/Widgets/ProgressBarWidget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PixelCrew.UI.Widgets
+{
+    public class ProgressBarWidget : MonoBehaviour
+    {
+        [SerializeField] private Image _image;
+        
+        public void SetProgress(float progress)
+        {
+            _image.fillAmount = progress;
+        }
+    }
+}
