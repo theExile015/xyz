987055edd81e547ccb6b9cf83b402af6bd87791a
theExile015 <theExile015@gmail.com>
2022-05-23 22:25:43 +0300

    mobile_controls

---
+++ Assets/PixelCrew/UI/MobileControls.cs
@@ -0,0 +1,67 @@
+﻿using PixelCrew.Creatures.Hero;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace PixelCrew.UI
+{
+    public class MobileControls : MonoBehaviour
+    {
+        [SerializeField] private MobileControl _control;
+
+        private Hero _hero;
+
+        private void TryFindHero()
+        {
+            if (_hero != null) return;
+            var hero = GameObject.FindObjectOfType<Hero>();
+            _hero = hero.GetComponent<Hero>();
+        }
+
+        public void OnControlDown()
+        {
+            TryFindHero();
+            switch (_control)
+            {
+                case MobileControl.ControlLeft:
+                    _hero.SetDirection(Vector2.left);
+                    break;
+
+                case MobileControl.ControlRight:
+                    _hero.SetDirection(Vector2.right);
+                    break;
+
+                case MobileControl.ControlA:
+                    _hero.SetDirection(Vector2.up);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void OnControlUp()
+        {
+            TryFindHero();
+            _hero.SetDirection(Vector2.zero);
+        }
+
+        private void OnDestroy()
+        {
+            _hero = null;
+        }
+
+        [Serializable]
+        public enum MobileControl
+        {
+            ControlLeft,
+            ControlRight,
+            ControlA,
+            ControlB,
+            ControlX,
+            ControlY,
+        }
+    }
+
+
+}
